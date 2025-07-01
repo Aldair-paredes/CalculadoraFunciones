40afd3c055bd68c7753349506b8883e93891e627
Georgina <reyesjoely448@gmail.com>
2025-07-01 17:27:22 -0600

    Se agrego limite a la función implicita

--- CalculadoraDeI/CalculadoraApp/funcion/implicita.py
+++ CalculadoraDeI/CalculadoraApp/funcion/implicita.py
@@ -44,18 +44,36 @@ class Funcion_implicita:
             print(f'Error para resolver y: {e}')
             return None
         
-    def derivada(self, respecto_a='y'):
+    def derivada(self, respecto_a='y', orden=1):
         try:
             if respecto_a == 'y':
-                expr_derivada= sp.diff(self.expresion, self.y)
+                expr_derivada= sp.diff(self.expresion, self.y, orden)
             elif respecto_a == 'x':
-                expr_derivada= sp.diff(self.expresion, self.x)
+                expr_derivada= sp.diff(self.expresion, self.x, orden)
             else:
                 raise ValueError("respecto_a debe ser 'x' o 'y'")
-            nueva_expresion= f'dF/d{respecto_a} = {str(expr_derivada)}'
+            nueva_expresion= f'df_{orden}/d{respecto_a} = {str(expr_derivada)}'
             nueva_funcion= Funcion_implicita(str(expr_derivada))
-            self.historial.append(('derivada', respecto_a, nueva_expresion))
+            self.historial.append(('derivada', respecto_a, orden, nueva_expresion))
             return nueva_funcion
         except Exception as e:
             print(f'Error al calcular derivada: {e}')
             return None
+        
+    def limite( self, variable, punto, direccion='+'):
+        try: 
+           var = self.x if variable == 'x' else self.y
+           lim = sp.limit(self.expresion, var, punto, dir=direccion)
+        
+           self.historial.append(('limite', variable, punto, direccion, lim))
+           return lim
+        except Exception as e:
+         print(f'Error al calcular límite: {e}')
+         return None
+
+    def simplificador(self):
+        try:
+            expr_simplificada = sp.simplify(self.expresion)
+            nueva_funcion = Funcion_implicita(str(expr_simplificada))
+            self.historial.append(('simplificar'), str(expr_simplificada))
+        except
