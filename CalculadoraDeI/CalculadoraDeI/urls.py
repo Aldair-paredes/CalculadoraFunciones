2d93d1bc6c34701137167cddb57c34aa9d01bb29
ArturoSantos06 <artsantoss30@gmail.com>
2025-07-05 14:20:59 -0600

    Termine funcion transcendente y añadi boton hamburgesa para el navbar

--- CalculadoraDeI/CalculadoraDeI/urls.py
+++ CalculadoraDeI/CalculadoraDeI/urls.py
@@ -11,6 +11,8 @@ path('', views.pagprincipal, name='pagprincipal'),
     path('calculadora_explicita/', views.calculadora_explicita, name='calculadora_explicita'),
     path('temas/', views.temas, name='temas'),
     path('tema_funciones/', views.tema_funciones, name='tema_funciones'),
+     path('calculadora-transcendente/', views.calculadora_transcendente, name='calculadora_transcendente'),
+
 
 
 
