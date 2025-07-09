12d329014b23edac9d5833a034ce6e5fa564b897
ArturoSantos06 <artsantoss30@gmail.com>
2025-07-09 13:46:35 -0600

    Funcion biyectiva, pendiente de revisar errores

--- CalculadoraDeI/CalculadoraApp/views.py
+++ CalculadoraDeI/CalculadoraApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .explicita import calcular_funcion_explicita 
 from .transcendente import calcular_funcion_transcendente 
+from .biyectiva import calcular_funcion_biyectiva
 
 
 def pagprincipal(request):
@@ -89,3 +90,48 @@ def calculadora_transcendente(request):
         'error': error,
     }
     return render(request, 'transcendente.html', context)
+
+
+
+
+def calculadora_biyectiva(request):
+    function_input = ""
+    operation_select = ""
+    variable_input = ""
+    limit_point_input = ""
+    evaluate_values_input = ""
+    result = None
+    error = None
+
+    if request.method == 'POST':
+        function_input = request.POST.get('function_input', '').strip()
+        operation_select = request.POST.get('operation_select', '').strip()
+        variable_input = request.POST.get('variable_input', '').strip()
+        limit_point_input = request.POST.get('limit_point_input', '').strip()
+        evaluate_values_input = request.POST.get('evaluate_values_input', '').strip()
+
+        kwargs_para_funcion = {}
+        if operation_select in ['derivar', 'limite', 'resolver']:
+            kwargs_para_funcion['variable_derivacion'] = variable_input 
+            kwargs_para_funcion['variable_integracion'] = variable_input 
+            kwargs_para_funcion['variable_limite'] = variable_input
+            kwargs_para_funcion['variable_resolver'] = variable_input
+        
+        if operation_select == 'limite':
+            kwargs_para_funcion['punto_limite'] = limit_point_input
+        
+        if operation_select == 'evaluar':
+            kwargs_para_funcion['valores_evaluacion_str'] = evaluate_values_input
+        
+        result, error = calcular_funcion_biyectiva(function_input, operation_select, **kwargs_para_funcion)
+    
+    context = {
+        'function_input': function_input,
+        'operation_select': operation_select,
+        'variable_input': variable_input, 
+        'limit_point_input': limit_point_input, 
+        'evaluate_values_input': evaluate_values_input, 
+        'result': result, 
+        'error': error,
+    }
+    return render(request, 'biyectiva.html', context)
--- CalculadoraDeI/CalculadoraDeI/urls.py
+++ CalculadoraDeI/CalculadoraDeI/urls.py
@@ -1,20 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from CalculadoraApp import views
+from CalculadoraApp import views # Asegúrate de que 'CalculadoraApp' es el nombre correcto de tu app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-
-
-path('', views.pagprincipal, name='pagprincipal'),
+    path('', views.pagprincipal, name='pagprincipal'),
     path('calculadora_explicita/', views.calculadora_explicita, name='calculadora_explicita'),
     path('temas/', views.temas, name='temas'),
     path('tema_funciones/', views.tema_funciones, name='tema_funciones'),
-     path('calculadora-transcendente/', views.calculadora_transcendente, name='calculadora_transcendente'),
-
-
-
-
-
+    path('calculadora-transcendente/', views.calculadora_transcendente, name='calculadora_transcendente'),
+    path('calculadora-biyectiva/', views.calculadora_biyectiva, name='calculadora_biyectiva'),
 ]
