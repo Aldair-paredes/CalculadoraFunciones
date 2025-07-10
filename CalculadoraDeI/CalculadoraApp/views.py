8a66af2c3747e6850300bec2d7033a06afdfb900
ArturoSantos06 <artsantoss30@gmail.com>
2025-07-09 20:44:02 -0600

    Solucion errores biyectiva

--- CalculadoraDeI/CalculadoraApp/views.py
+++ CalculadoraDeI/CalculadoraApp/views.py
@@ -246,3 +246,48 @@ def handle_radical_function(data):
             result['evaluation'] = f"f({val_x}) = {func_expr.subs(x, val_x)} (complejo)"
     
     return result
+
+def calculadora_biyectiva(request):
+
+    function_input = ""
+    operation_select = ""
+    variable_input = ""
+    limit_point_input = ""
+    evaluate_values_input = ""
+    result = None
+    error = None
+
+    if request.method == 'POST':
+        function_input = request.POST.get('function_input', '').strip()
+        operation_select = request.POST.get('operation_select', '').strip()
+        variable_input = request.POST.get('variable_input', '').strip()
+        limit_point_input = request.POST.get('limit_point_input', '').strip()
+        evaluate_values_input = request.POST.get('evaluate_values_input', '').strip()
+
+        kwargs_para_funcion = {}
+
+        if operation_select in ['derivar', 'limite', 'resolver']:
+            kwargs_para_funcion['variable_derivacion'] = variable_input 
+            kwargs_para_funcion['variable_integracion'] = variable_input 
+            kwargs_para_funcion['variable_limite'] = variable_input
+            kwargs_para_funcion['variable_resolver'] = variable_input
+        
+        if operation_select == 'limite':
+            kwargs_para_funcion['punto_limite'] = limit_point_input
+        
+        if operation_select == 'evaluar':
+            kwargs_para_funcion['valores_evaluacion_str'] = evaluate_values_input
+        
+
+        result, error = calcular_funcion_biyectiva(function_input, operation_select, **kwargs_para_funcion)
+    
+    context = {
+        'function_input': function_input,
+        'operation_select': operation_select,
+        'variable_input': variable_input, 
+        'limit_point_input': limit_point_input, 
+        'evaluate_values_input': evaluate_values_input, 
+        'result': result, 
+        'error': error,
+    }
+    return render(request, 'biyectiva.html', context)
--- CalculadoraDeI/CalculadoraDeI/settings.py
+++ CalculadoraDeI/CalculadoraDeI/settings.py
@@ -79,18 +79,12 @@ WSGI_APPLICATION = 'CalculadoraDeI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DBpreguntas',
-        'USER': 'proyecto',
-        'PASSWORD': 'pescadosanto50',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
