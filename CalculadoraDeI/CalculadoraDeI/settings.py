3411b7142306e3076c1d83d3ae0b5f6948835b42
AldairCastillo <Leonardo24aldair@gmail.com>
2025-07-28 17:28:56 -0600

    Codigo para sobreyectiva

--- CalculadoraDeI/CalculadoraDeI/settings.py
+++ CalculadoraDeI/CalculadoraDeI/settings.py
@@ -79,16 +79,8 @@ WSGI_APPLICATION = 'CalculadoraDeI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DBpreguntas',
-        'USER': 'proyecto',
-        'PASSWORD': 'pescadosanto50',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'Preguntas.db'
     }
 }
 
